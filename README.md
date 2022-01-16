32243f7e76a1ff1893a622b12ff6528811237ffb
bibhusprasad <bibhusprasad@gmail.com>
2022-01-16 11:45:30 +0530

    Internationalization(i18) Implemeted

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Section 3: Restful Web Services with Spring boot
 	commit: https://github.com/bibhusprasad/017_Udemy_Master_Microservices_with_Spring_Boot_and_Spring_Cloud/commit/de4240a78dad2f3288a060f14135d25ae967036a
 
 	scenario: HATEOAS Implemeted
+	commit: https://github.com/bibhusprasad/017_Udemy_Master_Microservices_with_Spring_Boot_and_Spring_Cloud/commit/da9b0f96359aa0cba6c93952bb9b73dbeb84ed29
+	
+	scenario: Internationalization(i18) Implemeted
 	commit: 
--- restfulwebservices/src/main/java/com/in28min/ws/restfulwebservices/controller/HelloWorldController.java
+++ restfulwebservices/src/main/java/com/in28min/ws/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.in28min.ws.restfulwebservices.controller;
 
-import com.in28min.ws.restfulwebservices.bean.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28min.ws.restfulwebservices.bean.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -21,5 +28,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable final String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return this.messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 }
