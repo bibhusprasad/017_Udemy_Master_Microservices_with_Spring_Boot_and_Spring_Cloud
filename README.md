da9b0f96359aa0cba6c93952bb9b73dbeb84ed29
bibhusprasad <bibhusprasad@gmail.com>
2022-01-16 11:24:33 +0530

    HATEOAS Implemeted

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # 017_Udemy_Master_Microservices_with_Spring_Boot_and_Spring_Cloud
 
 Section 3: Restful Web Services with Spring boot
+	
 	scenario: Basic Curd Operations
 	commit: https://github.com/bibhusprasad/017_Udemy_Master_Microservices_with_Spring_Boot_and_Spring_Cloud/commit/eb9809487afcbbb3a0155eeb464e19f8b11a388b
 	
-	
+	scenario: Validation Implemeted
+	commit: https://github.com/bibhusprasad/017_Udemy_Master_Microservices_with_Spring_Boot_and_Spring_Cloud/commit/de4240a78dad2f3288a060f14135d25ae967036a
+
+	scenario: HATEOAS Implemeted
+	commit: 
--- restfulwebservices/src/main/java/com/in28min/ws/restfulwebservices/controller/UserResource.java
+++ restfulwebservices/src/main/java/com/in28min/ws/restfulwebservices/controller/UserResource.java
@@ -1,16 +1,23 @@
 package com.in28min.ws.restfulwebservices.controller;
 
-import com.in28min.ws.restfulwebservices.bean.User;
-import com.in28min.ws.restfulwebservices.exception.UserNotFoundException;
-import com.in28min.ws.restfulwebservices.service.UserDaoService;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
+import com.in28min.ws.restfulwebservices.bean.User;
+import com.in28min.ws.restfulwebservices.exception.UserNotFoundException;
+import com.in28min.ws.restfulwebservices.service.UserDaoService;
 
 @RestController
 public class UserResource {
@@ -24,14 +31,16 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable final int id) {
+    public EntityModel<User> retrieveUser(@PathVariable final int id) {
         final User user = this.service.findOne(id);
-
         if (user == null) {
             throw new UserNotFoundException("id-" + id);
         }
 
-        return user;
+        final EntityModel<User> userModel = EntityModel.of(user);
+        final WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        userModel.add(linkBuilder.withRel("all-users"));
+        return userModel;
     }
 
     @DeleteMapping("/users/{id}")
